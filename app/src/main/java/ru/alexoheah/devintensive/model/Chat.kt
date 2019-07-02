a7149b76793d07db763ef1946d14bd052aa6bbe1
lexaxa <alexoheah@gmail.com>
2019-07-02 09:09:19 +0300

    Init hometask_2

--- app/src/main/java/ru/alexoheah/devintensive/model/Chat.kt
+++
@@ -1,9 +0,0 @@
-package ru.alexoheah.devintensive.model
-
-class Chat (
-    val id: String,
-    val members: MutableList<User> = mutableListOf(),
-    val messages: MutableList<BaseMessage> = mutableListOf()
-){
-
-}
