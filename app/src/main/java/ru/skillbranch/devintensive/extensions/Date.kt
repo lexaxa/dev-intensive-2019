115cdbe37ce72c8160cc67e8df09b8bb6bfa427c
lexaxa <alexoheah@gmail.com>
2019-07-02 13:40:32 +0300

    Fix humanizeDiff

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -16,6 +16,17 @@ fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"):String {
     return dateFormat.format(this)
 }
 
+/*
+0с - 1с "только что"
+1с - 45с "несколько секунд назад"
+45с - 75с "минуту назад"
+75с - 45мин "N минут назад"
+45мин - 75мин "час назад"
+75мин 22ч "N часов назад"
+22ч - 26ч "день назад"
+26ч - 360д "N дней назад"
+>360д "более года назад"
+ */
 fun Date.humanizeDiff(date: Date = Date()): String{
     val diff = "только что"
     val eventAfter : String
@@ -30,11 +41,14 @@ fun Date.humanizeDiff(date: Date = Date()): String{
     }
 
     return when (diffTime) {
-        in 0..5 -> "только что"
-        in 5..MINUTE -> "$eventAfter${Utils.parseTime(diffTime, TimeUnits.SECOND)}$eventBefore"
-        in MINUTE..HOUR -> "$eventAfter${Utils.parseTime(diffTime / MINUTE, TimeUnits.MINUTE)}$eventBefore"
-        in HOUR..DAY -> "$eventAfter${Utils.parseTime(diffTime / HOUR, TimeUnits.HOUR)}$eventBefore"
-        in DAY..365*DAY -> "$eventAfter${Utils.parseTime(diffTime / DAY, TimeUnits.DAY)}$eventBefore"
+        in 0..2*SECOND-1 -> "только что"
+        in 2*SECOND..45*SECOND -> "${eventAfter}несколько секунд$eventBefore"
+        in 46*SECOND..75*SECOND -> "${eventAfter}минуту$eventBefore"
+        in 76*SECOND .. 45*MINUTE -> "$eventAfter${Utils.parseTime(diffTime / MINUTE, TimeUnits.MINUTE)}$eventBefore"
+        in 45*MINUTE+1 .. 75*MINUTE -> "${eventAfter}час$eventBefore"
+        in 75*MINUTE+1 .. 22*HOUR -> "$eventAfter${Utils.parseTime(diffTime / HOUR, TimeUnits.HOUR)}$eventBefore"
+        in 22*HOUR+1 .. 26* HOUR -> "${eventAfter}день$eventBefore"
+        in 26*HOUR+1 .. 360*DAY -> "$eventAfter${Utils.parseTime(diffTime / DAY, TimeUnits.DAY)}$eventBefore"
         else -> "более ${if (eventAfter !="") "чем через год" else "года$eventBefore"}"
     }
 }
